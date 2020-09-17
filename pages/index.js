5ecc27a026cf94b39dcc730a3ac4e3a4084dc103
Samaneh Mohammadi <s.mohammadi@rashasoft.ir>
2020-09-17 18:21:13 +0430

    Project config

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import Link from 'next/link';
+import Baloot from '../icons/baloot-logo.svg';
 
 export default function Home() {
   return (
@@ -12,40 +14,29 @@ export default function Home() {
         <h1 className="title">
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
         <p className="description">
           Get started by editing <code>pages/index.js</code>
+            <Baloot/>
         </p>
 
         <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <Link href="/about" className="card">
+            <div>
+            <h3>About &rarr;</h3>
+            <p>about</p>
+            </div>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
+          <div className="grid">
+              <Link href="/f/dashoori" className="card">
+                  <div>
+                      <h3>Dash &rarr;</h3>
+                      <p>go to dashoori</p>
+                  </div>
+              </Link>
+
+          </div>
       </main>
 
       <footer>
